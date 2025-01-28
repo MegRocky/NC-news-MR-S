6156e57e9648c8d3bb8b06bda1238d24f275d934
Megrocky <megrocky63@gmail.com>
2025-01-28 10:33:07 +0000

    test and add GET:/api/articles endpoint

--- models/model-select-article.js
+++ models/model-select-article.js
@@ -13,4 +13,14 @@ function selectArticleById(id) {
     });
 }
 
-module.exports = selectArticleById;
+function selectArticles() {
+  return db
+    .query(
+      "SELECT articles.author,title,articles.article_id,topic,articles.created_at,articles.votes,article_img_url,CAST(COUNT(comments.comment_id) AS int) AS comment_count  FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY (articles.article_id) ORDER BY articles.created_at DESC ;"
+    )
+    .then((res) => {
+      return res.rows;
+    });
+}
+
+module.exports = { selectArticleById, selectArticles };
