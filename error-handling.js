87ca988c9ed8bb03ff6de19b4c7a12815801f9b5
Megrocky <megrocky63@gmail.com>
2025-01-29 09:40:03 +0000

    fix task 7 tests

--- error-handling.js
+++ error-handling.js
@@ -1,15 +1,14 @@
 exports.badRequest = (err, req, res, next) => {
-  if (err.code === "22P02" || err.code === "23502" || err.code === "23503") {
+  if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "Bad Request" });
   } else next(err);
 };
 
 exports.idNotFound = (err, req, res, next) => {
-  if (err.msg === "Article Not Found") {
-    res.status(404).send({ msg: err.msg });
+  if (err.msg && err.status) {
+    res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
-
 exports.internalServerError = (err, req, res, next) => {
   res.status(500).send({ msg: "Internal server error!" });
 };
--- models/model-utils.js
+++ models/model-utils.js
@@ -1,3 +1,4 @@
+const { string } = require("pg-format");
 const db = require("../db/connection.js");
 
 function checkIfArticleExists(id) {
@@ -10,4 +11,18 @@ function checkIfArticleExists(id) {
     });
 }
 
-module.exports = { checkIfArticleExists };
+function checkIfValidUserExists(username) {
+  if (typeof username !== "string") {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  } else {
+    return db
+      .query("SELECT * FROM users WHERE username = $1", [username])
+      .then((userQ) => {
+        if (userQ.rowCount === 0) {
+          return Promise.reject({ status: 404, msg: "User Not Found" });
+        }
+      });
+  }
+}
+
+module.exports = { checkIfArticleExists, checkIfValidUserExists };
