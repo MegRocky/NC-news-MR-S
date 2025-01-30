4ca052a16b4122d675320ef711a9e7ac048a2762
Megrocky <megrocky63@gmail.com>
2025-01-30 11:59:06 +0000

    refactor to user express router

---
+++ routes/api-router.js
@@ -0,0 +1,15 @@
+const apiRouter = require("express").Router();
+const articleRouter = require("./articles-router.js");
+const topicsRouter = require("./topics-router.js");
+const commentsRouter = require("./comments-router.js");
+const usersRouter = require("./users-router.js");
+
+const { getApi } = require("../controllers/control-get-api.js");
+
+apiRouter.get("/", getApi);
+apiRouter.use("/articles", articleRouter);
+apiRouter.use("/topics", topicsRouter);
+apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
+
+module.exports = apiRouter;
